9d775d4cffd8001ad009edf6c1b845af2189e80a
hkgnp <hkgnp@github.com>
2023-02-27 00:35:37 +0800

    [fix] time in ampm is wrong is time is midnight

--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -7,7 +7,14 @@ export default function getTime(flag?: string) {
     return `${hours}:${mins}`;
   } else if (flag === "ampm") {
     const ampm = d.getHours() > 11 ? "pm" : "am";
-    const newHours = d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+    let newHours: number;
+    if (d.getHours() === 0) {
+      newHours = 12;
+    } else if (d.getHours() > 12) {
+      newHours = d.getHours() - 12;
+    } else {
+      newHours = d.getHours();
+    }
     return `${newHours}:${d.getMinutes()} ${ampm}`;
   }
 }
