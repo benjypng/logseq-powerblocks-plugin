b17fe9bf2d3bce9e67268a5c598f28ba2177bf5b
Benjy <ben@pngs.cc>
2024-07-14 16:42:37 +0800

    fix: typings

--- src/services/checkIfCondition.ts
+++ src/services/checkIfCondition.ts
@@ -1,57 +1,58 @@
-import * as chrono from "chrono-node";
-import { getWeek, getWeekOfMonth } from "date-fns";
+import * as chrono from 'chrono-node'
+import { getWeek, getWeekOfMonth } from 'date-fns'
 
-import getDateInYYYYMMDD from "../utils/getDateInYYYYMMDD";
+import getDateInYYYYMMDD from '../utils/getDateInYYYYMMDD'
 
 export default function checkIfCondition(matched: string, nlp?: boolean) {
   const checkerObj = {
-    ["IFDAYOFWEEK"]: "day",
-    ["IFMONTHOFYEAR"]: "month",
-    ["IFYEAR"]: "year",
-    ["IFWEEKOFMONTH"]: "weekofmonth",
-    ["IFWEEKOFYEAR"]: "weekofyear",
-  };
+    ['IFDAYOFWEEK']: 'day',
+    ['IFMONTHOFYEAR']: 'month',
+    ['IFYEAR']: 'year',
+    ['IFWEEKOFMONTH']: 'weekofmonth',
+    ['IFWEEKOFYEAR']: 'weekofyear',
+  }
 
   if (!nlp) {
-    let comparator: number;
-    // @ts-ignore
-    switch (checkerObj[matched.split(":")[0]!]) {
-      case "day":
-        comparator = new Date().getDay();
-        break;
+    let comparator: number
+    // @ts-expect-error Type because of indexing
+    switch (checkerObj[matched.split(':')[0]!]) {
+      case 'day':
+        comparator = new Date().getDay()
+        break
 
-      case "month":
-        comparator = new Date().getMonth();
-        break;
+      case 'month':
+        comparator = new Date().getMonth()
+        break
 
-      case "year":
-        comparator = new Date().getFullYear();
-        break;
+      case 'year':
+        comparator = new Date().getFullYear()
+        break
 
-      case "weekofmonth":
-        comparator = getWeekOfMonth(new Date());
-        break;
+      case 'weekofmonth':
+        comparator = getWeekOfMonth(new Date())
+        break
 
-      case "weekofyear":
-        comparator = getWeek(new Date());
-        break;
+      case 'weekofyear':
+        comparator = getWeek(new Date())
+        break
 
       default:
-        return false;
+        return false
     }
 
-    let state = false;
-    const checker = matched.split(":")[1]!.split(",");
+    let state = false
+    const checker = matched.split(':')[1]!.split(',')
 
     for (const i of checker) {
       if (comparator === parseInt(i)) {
-        state = true;
+        state = true
       }
     }
 
-    return state;
+    return state
   } else {
-    const dateToCheck = chrono.parseDate(matched.split(":")[1]!);
-    return getDateInYYYYMMDD(dateToCheck) === getDateInYYYYMMDD(new Date());
+    const dateToCheck = chrono.parseDate(matched.split(':')[1]!)
+    if (!dateToCheck) return false
+    return getDateInYYYYMMDD(dateToCheck) === getDateInYYYYMMDD(new Date())
   }
 }
