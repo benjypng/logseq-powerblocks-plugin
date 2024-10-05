37fe2804439acd344ba0136ca0a3d2145517b4c0
benjypng <47936793+benjypng@users.noreply.github.com>
2024-10-05 09:27:37 +0800

    fix: ifdayofweek

--- src/libs/handleIfDayOfWeek.ts
+++ src/libs/handleIfDayOfWeek.ts
@@ -1,16 +1,16 @@
-import checkIfCondition from "../services/checkIfCondition";
+import checkIfCondition from '../services/checkIfCondition'
 
 export const handleIfDayOfWeek = (content: string): string => {
-  if (content.includes("<%IFDAYOFWEEK:") && content.includes("%>")) {
-    const regexp = /<%(.*?)%>/;
-    const matched = regexp.exec(content);
-    if (!matched || !matched[1]) return content;
+  if (content.includes('<%IFDAYOFWEEK:') && content.includes('%>')) {
+    const regexp = /<%(.*?)%>/
+    const matched = regexp.exec(content)
+    if (!matched || !matched[1]) return content
 
     if (checkIfCondition(matched[1])) {
-      return content.replaceAll(matched![0], "");
+      return content.replaceAll(matched![0], '')
     } else {
-      return content;
+      return ''
     }
   }
-  return content;
-};
+  return content
+}
